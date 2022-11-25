a5dbd78aed97d77442961468a029f003ad7dc0e3
Rokas <rookas.rudzenskas@gmail.com>
2022-11-25 18:37:12 +0200

    Add Rokas to the event name
    I have changed the event name to Rokas.Hack

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,36 @@
 //@ts-nocheck
-import { StyleSheet } from 'react-native';
+import {Alert, StyleSheet, TouchableOpacity} from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import {StatusBar} from "expo-status-bar";
-import {Agenda} from "react-native-calendars/src";
+import {Agenda, AgendaEntry} from "react-native-calendars/src";
+import events from "../assets/data/events.json";
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
+    const fontSize = isFirst ? 15 : 13;
+    const color = isFirst ? "black" : "#43515c";
+
+    return (
+        <TouchableOpacity
+          style={[styles.item, {height: reservation.height}]}
+          onPress={() => Alert.alert("Reservation", reservation.name)}
+          activeOpacity={0.7}
+          className="shadow-sm mt-3"
+        >
+          <Text className="text-gray-500" style={{ fontSize, color }}>{reservation.name}</Text>
+        </TouchableOpacity>
+    )
+
+  }
+
   return (
       <View className="flex-1 bg-white pt-16">
-        <Agenda />
+        <Agenda
+          items={events}
+          renderItem={renderItem}
+        />
         <StatusBar style="auto" />
       </View>
   );
