d952a16b7f8b2ec13afdc333d69072b35628d6dc
Rokas <rookas.rudzenskas@gmail.com>
2022-11-27 18:42:56 +0200

    Added the chat func

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
@@ -6,6 +7,9 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import { NhostClient, NhostReactProvider } from '@nhost/react';
 import * as SecureStore from 'expo-secure-store';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+// import { OverlayProvider } from 'stream-chat-react-native';
+
 import {NhostApolloProvider} from "@nhost/react-apollo";
 
 export default function App() {
@@ -23,14 +27,18 @@ export default function App() {
     return null;
   } else {
     return (
+        <GestureHandlerRootView style={{flex: 1}}>
+          {/*<OverlayProvider>*/}
       <SafeAreaProvider>
-        <NhostReactProvider nhost={nhost}>
-          <NhostApolloProvider nhost={nhost}>
-            <Navigation colorScheme={colorScheme} />
-            <StatusBar />
-          </NhostApolloProvider>
-        </NhostReactProvider>
+            <NhostReactProvider nhost={nhost}>
+              <NhostApolloProvider nhost={nhost}>
+                <Navigation colorScheme={colorScheme} />
+                <StatusBar />
+              </NhostApolloProvider>
+            </NhostReactProvider>
       </SafeAreaProvider>
+          {/*</OverlayProvider>*/}
+        </GestureHandlerRootView>
     );
   }
 }
