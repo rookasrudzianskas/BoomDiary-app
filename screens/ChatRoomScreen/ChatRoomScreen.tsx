376a40d14ea735074286fa86fc3ac0ce56d9b3d1
Rokas <rookas.rudzenskas@gmail.com>
2022-11-28 09:28:09 +0200

    Rokas

--- screens/ChatRoomScreen/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen/ChatRoomScreen.tsx
@@ -1,14 +1,22 @@
 //@ts-nocheck
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {useChatContext} from "../../contexts/ChatContext";
 import {Channel, MessageList, MessageInput} from "stream-chat-expo";
 import { LogBox } from 'react-native';
+import {useNavigation} from "@react-navigation/native";
 
 LogBox.ignoreAllLogs();//Ignore all log notifications
 
 const ChatRoomScreen = () => {
     const { currentChannel } = useChatContext();
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: currentChannel?.data?.name || 'Chat',
+        })
+    }, [currentChannel?.data?.name]);
 
     return (
         <Channel channel={currentChannel}>
